2a3c68d61d34ce470cd9edf60bc611e1b710d438
hendriam <hendriam18@gmail.com>
2025-06-21 00:04:38 +0700

    refactor: update navigation for user authentication with dropdown menu and logout button

--- resources/views/partials/app/nav.blade.php
+++ resources/views/partials/app/nav.blade.php
@@ -13,12 +13,52 @@
 
 	
 	@if(!auth()->user())
-		<a href="{{ route('login') }}" class="px-4 py-2.5 text-sm font-medium text-white bg-primary hover:bg-primary/90 hover:cursor-pointer">Login</a>
+		<div class="flex space-x-2">
+			<a href="{{ route('login') }}" class="px-4 py-2.5 text-sm font-medium text-secondary bg-white hover:bg-gray-50 hover:cursor-pointer">Login</a>
+			<a href="{{ route('register') }}" class="px-4 py-2.5 text-sm font-medium text-white bg-primary hover:bg-primary/90 hover:cursor-pointer">Register</a>
+		</div>
 	@else
-		<form action="{{ route('logout') }}" method="post">
+		<button
+			type="button"
+			class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+			id="user-menu-button"
+			aria-expanded="false"
+			data-dropdown-toggle="dropdown"
+		>
+			<span class="sr-only">Open user menu</span>
+			<img class="w-10 h-10 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png" alt="user photo" />
+		</button>
+
+		<!-- Dropdown menu -->
+		<div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
+			<div class="py-3 px-4">
+				<span class="block text-sm font-semibold text-gray-900 dark:text-white">{{ auth()->user()->fullname }}</span>
+				<span class="block text-sm text-gray-900 truncate dark:text-white">{{ auth()->user()->email }}</span>
+			</div>
+			<ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
+				<li>
+					<a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My profile</a>
+				</li>
+				<li>
+					<a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Account settings</a>
+				</li>
+			</ul>
+			
+			<ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
+				<li  class="w-full">
+					<form action="{{ route('logout') }}" method="post">
+						@csrf
+						<button type="submit" class="w-full text-start py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:cursor-pointer">Logout</button> 
+					</form>
+					<!-- <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a> -->
+				</li>
+			</ul>
+		</div>
+
+		<!-- <form action="{{ route('logout') }}" method="post">
 			@csrf
 	        <x-ui.button-primary type="submit" class="w-full p-2.5">Logout</x-ui.button-primary>
-		</form>
+		</form> -->
 	@endif
 
 </nav>
