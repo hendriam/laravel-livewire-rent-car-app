b400d518f9b5aa9430cf3c9bc6c9d87d0aecdbb7
hendriam <hendriam18@gmail.com>
2025-06-20 15:02:38 +0700

    feat: implement login functionality with validation and error handling

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -5,11 +5,32 @@ namespace App\Livewire\Auth;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
+use Illuminate\Support\Facades\Auth;
 
 #[Layout('components.layouts.auth')]
 class Login extends Component
 {
     #[Title('Login - Rent.Car')] 
+
+    public string $email = '';
+    public string $password = '';
+    public bool $remember = false;
+
+    public function login(): void
+    {
+        $this->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->remember)) {
+            session()->regenerate();
+            // return redirect()->intended('/dashboard');
+            $this->redirectIntended(default: route('home', absolute: false), navigate: true);
+        }
+
+        $this->addError('email', 'Email atau password salah.');
+    }
 
     public function render()
     {
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,19 +1,21 @@
 <section class="bg-white min-w-sm shadow py-8 px-9 mt-6">
     <h3 class="text-xl font-semibold text-secondary">Sign in to your account</h3>
-    <form class="mt-6 flex flex-col space-y-4">
+    <form wire:submit.prevent="login" class="mt-6 flex flex-col space-y-4" autocomplete="off">
         <div>
             <x-ui.label for="email">Email</x-ui.label>
-            <x-ui.input type="text" name="email" wire:model.defer="email" class="w-full p-2.5" placeholder="test@example.com" required></x-ui.input>
+            <x-ui.input type="text" wire:model.defer="email" class="w-full p-2.5" placeholder="test@example.com" required></x-ui.input>
+            @error('email')<p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{ $message }}!</p>@enderror
         </div>
         <div>
             <x-ui.label for="password">Password</x-ui.label>
-            <x-ui.input type="password" name="password" wire:model.defer="password" class="w-full p-2.5" placeholder="*********" required></x-ui.input>					
+            <x-ui.input type="password" wire:model.defer="password" class="w-full p-2.5" placeholder="*********" required></x-ui.input>					
+            @error('password')<p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{ $message }}!</p>@enderror
         </div>
 
         <div class="flex items-center justify-between">
             <div class="flex items-start">
                 <div class="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-1 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
+                    <input id="remember" wire:model.defer="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-1 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
                 </div>
                 <div class="ml-3 text-sm">
                     <label for="remember" class="text-gray-700 dark:text-gray-300">Remember me</label>
