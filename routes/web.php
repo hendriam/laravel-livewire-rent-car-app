164032d1f8178750bd955659a845de0cebadb03b
hendriam <hendriam18@gmail.com>
2025-06-20 22:52:47 +0700

    refactor: change logout function to use controller instead livewire

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Livewire\App\Home;
 use App\Livewire\Customer\Auth\Login as CustomerLogin;
 use App\Livewire\Customer\Auth\Register as CustomerRegister;
 use App\Livewire\Customer\Auth\ForgotPassword as CustomerForgetPassword;
-use App\Livewire\Customer\Auth\Logout as CustomerLogout;
+use App\Http\Controllers\LogoutController;
 
 Route::get('/', Home::class)->name('home');
 
@@ -13,4 +13,4 @@ Route::get('login', CustomerLogin::class)->name('login');
 Route::get('register', CustomerRegister::class)->name('register');
 Route::get('forgot-password', CustomerForgetPassword::class)->name('forgot-password');
 
-Route::post('logout', [CustomerLogout::class, 'logout'])->name('logout');    
+Route::post('logout', [LogoutController::class, 'logout'])->name('logout');    
