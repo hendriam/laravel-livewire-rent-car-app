7ecfdd4375a586ae46f9c6e883ff06f9ee52db18
hendriam <hendriam18@gmail.com>
2025-06-25 19:10:45 +0700

    feat: add Car model and migration for cars table

---
+++ app/Models/Car.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    protected $fillable = [
+        'brand',
+        'model',
+        'plate_number',
+        'year',
+        'status',
+        'price_without_driver',
+        'price_with_driver',
+        'photo',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
