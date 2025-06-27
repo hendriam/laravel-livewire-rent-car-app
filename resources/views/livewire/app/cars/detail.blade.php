970037a550b167b0932877523105d7975af8b964
hendriam <hendriam18@gmail.com>
2025-06-27 14:28:02 +0700

    feat: add button action booking detail page

--- resources/views/livewire/app/cars/detail.blade.php
+++ resources/views/livewire/app/cars/detail.blade.php
@@ -7,8 +7,32 @@
                 <h2 class="font-medium text-gray-500 dark:text-gray-400">Plat {{ $car->plate_number }}</h2>
                 <h2 class="font-medium text-gray-500 dark:text-gray-400">Warna {{ $car->color }}</h2>
                 <h2 class="font-medium text-gray-500 dark:text-gray-400">Tahun {{ $car->year }}</h2>
+
+                <div class="md:flex hidden sm:justify-normal justify-center space-x-3 mt-4">
+                    <div class="flex flex-col">
+                        <span class="text-[#4B4B4B] font-medium">Tanpa Supir</span>
+                        <x-ui.button-primary type="button" class="mt-1 px-2 w-full lg:max-w-fit">Rp. {{ number_format($car->price_without_driver, 0, ',', '.') }} / Hari</x-ui.button-primary>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <span class="text-[#4B4B4B] font-medium ">Ikut supir</span>
+                        <x-ui.button-primary type="button" class="mt-1 px-2 w-full lg:max-w-fit">Rp. {{ number_format($car->price_with_driver, 0, ',', '.') }} / Hari</x-ui.button-primary>
+                    </div>
+                </div>
             </div>
             <img src="{{ $car->photo ? asset('storage/' . $car->photo) : asset('assets/images/default-car.png') }}"  class="" alt="vector-1" srcset="">
-        </div>  
+        </div>
+
+        <div class="flex md:hidden sm:justify-normal justify-center space-x-3 mt-4">
+            <div class="flex flex-col">
+                <span class="text-[#4B4B4B] font-medium">Tanpa Supir</span>
+                <x-ui.button-primary type="button" class="mt-1 px-2 w-full lg:max-w-fit">Rp. {{ number_format($car->price_without_driver, 0, ',', '.') }} / Hari</x-ui.button-primary>
+            </div>
+
+            <div class="flex flex-col">
+                <span class="text-[#4B4B4B] font-medium ">Ikut supir</span>
+                <x-ui.button-primary type="button" class="mt-1 px-2 w-full lg:max-w-fit">Rp. {{ number_format($car->price_with_driver, 0, ',', '.') }} / Hari</x-ui.button-primary>
+            </div>
+        </div>
     </div>
 </section>
